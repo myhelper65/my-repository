5f6731b13454ef261c747b9a0252e11bbdebbc92
myhelper65 <myhelper65@gmail.com>
2024-08-12 20:43:33 -0400

    second commit

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
@@ -1,26 +1,33 @@
 #Import Flask modules
-
+from flask import Flask, render_template, redirect, url_for
 
 #Create an object named app 
+app = Flask(__name__)
 
 
 # Create a function named home which returns a string 'This is home page for no path, <h1> Welcome Home</h1>' 
 # and assign route of no path ('/')
-
+@app.route("/")
+def home():
+    return "This is home page for no path, <h1> Welcome Home</h1>"
 
 
 # Create a function named about which returns a formatted string '<h1>This is my about page </h1>' 
 # and assign to the static route of ('about')
-
-
+@app.route("/about")
+def about():
+    return "<h1> This is my about page </h1>"
 # Create a function named error which returns a formatted string '<h1>Either you encountered an error or you are not authorized.</h1>' 
 # and assign to the static route of ('error')
-
+@app.route("/error")
+def error():
+    return "<h1>Either you encountered an error or you are not authorized.</h1>"
 
 # Create a function named admin which redirect the request to the error path 
 # and assign to the route of ('/admin')
-
-
+@app.route("/admin")
+def admin():
+     return redirect(url_for("error"))
 # Create a function named greet which return formatted inline html string 
 # and assign to the dynamic route of ('/<name>')
 # HTML:
@@ -41,13 +48,17 @@
 # Create a function named greet_admin which redirect the request to the hello path with param of 'Master Admin!!!!' 
 # and assign to the route of ('/greet-admin')
 
-
+@app.route("/greet-admin")
+def greet_admin():
+      return redirect(url_for("greet",username="Master Admin!!!"))
 
 # Rewrite a function named greet which uses template file named `greet.html` under `templates` folder 
 # and assign to the dynamic route of ('/<name>'). 
 # Please find a template html file named `greet.html` which takes `name` as parameter under `templates` folder 
 
-
+@app.route("/<username>")
+def greet(username):
+     return render_template("greet.html", name = username)
 
 
 # Create a function named list10 which creates a list counting from 1 to 10 within `list10.html` 
@@ -62,3 +73,5 @@
 
 
 # Add a statement to run the Flask application
+if __name__== "__main__":
+    app.run(debug=True)
