429ca1c44cdae073f825c60d7e25dbbe70f4ce95
myhelper65 <myhelper65@gmail.com>
2024-08-13 14:51:29 -0400

    edit some information on 1st app.py

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -1,11 +1,11 @@
 # Import Flask modules
 from flask import Flask, render_template
 
-# Create an object named app 
+# Create an object named app
 app =Flask(__name__)
 
 
-# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
+# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html`
 # and assign to the route of ('/')
 
 @app.route("/")
@@ -13,8 +13,8 @@ def head():
      first="This is my first conditions experience"
      return render_template("index.html", message=first)
 
-# Create a function named header which prints numbers elements of list one by one in `index.html` 
-# and assign to the route of ('/')
+# Create a function named header which prints numbers elements of list one by one in `body.html`
+# and assign to the route of ('/mylist')
 
 @app.route("/mylist")
 def header():
